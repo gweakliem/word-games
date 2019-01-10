ceed20b8a54b78dc05f0a52b30aeffc7f69afe51
Marshall Pierce <marshall@mpierce.org>
2019-01-10 09:02:52 -0700

    Use synchronization so multi-operation methods are atomic.
    (CHM can't help with being atomic across two operations.)

--- src/test/kotlin/org/mpierce/ktordemo/MemoryWidgetDao.kt
+++ src/test/kotlin/org/mpierce/ktordemo/MemoryWidgetDao.kt
@@ -1,28 +1,31 @@
 package org.mpierce.ktordemo
 
 import java.time.Instant
-import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.atomic.AtomicInteger
 
 /**
  * An in-memory implementation of WidgetDao to use when testing other logic that needn't use an actual database.
  */
 class MemoryWidgetDao : WidgetDao {
-    private val widgets = ConcurrentHashMap<Int, Widget>()
+    private val widgets = mutableMapOf<Int, Widget>()
     private val nextId = AtomicInteger()
 
+    @Synchronized
     override fun getWidget(id: Int): Widget? = widgets[id]
 
+    @Synchronized
     override fun getAllWidgets(): List<Widget> {
         return widgets.values.toList()
     }
 
+    @Synchronized
     override fun createWidget(name: String): Widget {
         val w = Widget(nextId.getAndIncrement(), name, Instant.now())
         widgets[w.id] = w
         return w
     }
 
+    @Synchronized
     override fun updateWidgetName(id: Int, name: String): Widget {
         val widget = widgets[id]!!.copy(name = name)
         widgets[id] = widget
