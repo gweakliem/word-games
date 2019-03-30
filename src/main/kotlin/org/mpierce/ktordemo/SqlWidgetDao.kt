69b54013617900f73c1d7ffb445ca8884962a4b8
Marshall Pierce <marshall@mpierce.org>
2019-03-29 21:39:01 -0600

    Improve sql dao example

--- src/main/kotlin/org/mpierce/ktordemo/SqlWidgetDao.kt
+++ src/main/kotlin/org/mpierce/ktordemo/SqlWidgetDao.kt
@@ -13,6 +13,7 @@ class SqlWidgetDao(private val txnContext: DSLContext) : WidgetDao {
     }
 
     override fun getAllWidgets(): List<Widget> {
+        // selecting from a table yields typed `WidgetsRecord` objects -- no raw ResultSet wrangling
         return txnContext.selectFrom(WIDGETS)
                 .orderBy(WIDGETS.ID.asc())
                 .fetch()
@@ -21,15 +22,33 @@ class SqlWidgetDao(private val txnContext: DSLContext) : WidgetDao {
     }
 
     override fun createWidget(name: String): Widget {
-        val result = txnContext.insertInto(WIDGETS, WIDGETS.NAME)
-                .values(name)
-                .returning()
-                .fetchOne()
+        // This method returns a Widget, which includes a `createdAt` timestamp set by the database.
+        // In PostgreSQL, we can use the RETURNING clause to insert a new record and get back the db-generated
+        // primary key and timestamp as follows, which produces a fully populated WidgetsRecord:
+        /*
+            val result = txnContext.insertInto(WIDGETS, WIDGETS.NAME)
+            .values(name)
+            .returning()
+            .fetchOne()
+         */
+        // See https://www.jooq.org/doc/3.11/manual/sql-building/sql-statements/insert-statement/insert-returning/.
+        // However, just to show how to do it with other databases that lack support for that, here we'll use a
+        // WidgetsRecord to create a new row with a separate INSERT and SELECT.
 
-        return Widget(result)
+        val record = txnContext.newRecord(WIDGETS).apply {
+            this.name = name
+            store()
+            // the db row exists, but this object doesn't know about db-generated data, so we explicitly refresh
+            refresh()
+        }
+
+        return Widget(record)
     }
 
     override fun updateWidgetName(id: Int, name: String): Widget {
+        // Just as a demonstration, we'll use a different style from `createWidget()`.
+        // You can also use `WidgetsRecord` for this, though:
+        // https://www.jooq.org/doc/3.11/manual/sql-execution/crud-with-updatablerecords/simple-crud/
         val result = txnContext.update(WIDGETS)
                 .set(WIDGETS.NAME, name)
                 .where(WIDGETS.ID.eq(id))
